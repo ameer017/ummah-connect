12f51ba01b3c73dd67ad1d43b9849a8b48218beb
ameer017 <rajiabdullahi907@gmail.com>
2024-07-03 22:22:06 +0100

    lc-3/7

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -19,6 +19,12 @@ import CreateThread from "./components/Forum/CreateThread";
 import ReportThread from "./components/Forum/ReportThread";
 import ThreadDetail from "./components/Forum/ThreadDetail";
 import VideoUpload from "./components/Content/VideoUpload";
+import EventList from "./components/Event/EventList";
+import CourseList from "./components/Courses/CourseList";
+import EventDetails from "./components/Event/EventDetails";
+import EventCreate from "./components/Event/EventCreate";
+import MentorshipList from "./components/Courses/MentoshipList";
+import MentorshipForm from "./components/Courses/Mentorship";
 
 const AppRoutes = () => (
   <Routes>
